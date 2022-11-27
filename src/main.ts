4248310d9478b28c8fe426230c0d83fe63bb6b9b
sturquier <turquiersimon@hotmail.fr>
2022-11-27 19:42:13 +0100

    Setup architecture

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+	NestFastifyApplication,
+	FastifyAdapter,
+} from '@nestjs/platform-fastify';
+import { join } from 'path';
+import { handlebars } from 'hbs';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+
+	app.useStaticAssets({
+		root: join(__dirname, '..', 'public'),
+	});
+
+	app.setViewEngine({
+		engine: {
+			handlebars,
+		},
+		templates: join(__dirname, '..', 'views'),
+	});
+
+	await app.listen(3000);
 }
 bootstrap();
